b1ee481d042788b320e7ce7c134f0ec83a7b4d0a
jeon jun ik <jji3379@naver.com>
2021-01-06 16:41:22 +0900

    insert, getList, getData, delete, update, addViewCount

---
+++ Step06_Final/src/test/cafe/dao/CafeDao.java
@@ -0,0 +1,236 @@
+package test.cafe.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.cafe.dto.CafeDto;
+import test.util.DbcpBean;
+
+public class CafeDao {
+	private static CafeDao dao;
+	private CafeDao() {}
+	public static CafeDao getInstance() {
+		if(dao==null) {
+			dao=new CafeDao();
+		}
+		return dao;
+	}//getInstance END
+	
+	//글 하나의 정보를 추가하는 메소드
+	public boolean insert(CafeDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "INSERT INTO board_cafe"
+					+ " VALUES(board_cafe_seq.NEXTVAL, ?, ?, ?, 0, SYSDATE)";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getTitle());
+			pstmt.setString(3, dto.getContent());
+			flag = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}//insert END
+	
+	//글 전체 목록을 리턴하는 메소드
+	public List<CafeDto> getList(){
+		//리턴해줄 ArrayList 객체 생성
+		List<CafeDto> list = new ArrayList<CafeDto>();
+				
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "SELECT num, writer, title, viewCount, regdate"
+					+ " FROM board_cafe"
+					+ " ORDER BY num DESC";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할게 있으면 여기서 바인딩한다.
+
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet 으로 부터 data 추출
+			while (rs.next()) {
+				CafeDto dto2 = new CafeDto();
+				dto2.setNum(rs.getInt("num"));
+				dto2.setWriter(rs.getString("writer"));
+				dto2.setTitle(rs.getString("title"));
+				dto2.setViewCount(rs.getInt("viewCount"));
+				dto2.setRegdate(rs.getString("regdate"));
+				//ArrayList 객체에 누적 시킨다.
+				list.add(dto2);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)rs.close();
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}//getList END
+	
+	//인자로 전달되는 글 번호에 해당하는 글정보를 리턴하는 메소드
+	public CafeDto getData(int num) {
+		CafeDto dto=null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "SELECT writer, title, content, viewCount, regdate"
+					+ " FROM board_cafe"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setInt(1, num);
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet 으로 부터 data 추출
+			if (rs.next()) {
+				dto=new CafeDto();
+				dto=new CafeDto();
+				dto.setNum(num);
+				dto.setWriter(rs.getString("writer"));
+				dto.setTitle(rs.getString("title"));
+				dto.setContent(rs.getString("content"));
+				dto.setViewCount(rs.getInt("viewCount"));
+				dto.setRegdate(rs.getString("regdate"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)rs.close();
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return dto;
+	}//getData END
+	
+	//인자로 전달되는 글번호를 이용해서 삭제하는 메소드
+	public boolean delete(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "DELETE FROM board_cafe"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setInt(1, num);
+			flag = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}//delete END
+	
+	//인자로 전달되는 글 내용을 수정 반영하는 메소드
+	public boolean update(CafeDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "UPDATE board_cafe"
+					+ " SET title=?, content=?"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setString(2, dto.getContent());
+			pstmt.setInt(3, dto.getNum());
+			flag = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}//update END
+	
+	//글 조회수를 올리는 메소드
+	public boolean addViewCount(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "UPDATE board_cafe"
+					+ " SET viewCount=viewCount+1"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setInt(1, num);
+			flag = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}//addViewCount END
+	
+}//Class END
