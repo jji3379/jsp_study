02b7ed7ff5b5552f73f86fc977457d86ac3d0569
jeon jun ik <jji3379@naver.com>
2021-01-07 17:27:22 +0900

    getList, insert, getData

---
+++ Step06_Final/src/test/file/dao/FileDao.java
@@ -0,0 +1,142 @@
+package test.file.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.file.dto.FileDto;
+import test.util.DbcpBean;
+
+public class FileDao {
+	//static필드
+	private static FileDao dao;	
+	//외부에서 객체 생성하지 못하도록 생성자를 private 로
+	private FileDao() {}
+	//자신의 참조값을 리턴해주는 메소드
+	public static FileDao getInstance() {
+		if(dao==null) {
+			dao=new FileDao();
+		}
+		return dao;
+	}
+	public List<FileDto> getList(){
+		List<FileDto> list=new ArrayList<FileDto>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "SELECT num, writer, title, orgFileName, fileSize, regdate"
+					+ " FROM board_file"
+					+ " ORDER BY num DESC";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할게 있으면 여기서 바인딩한다.
+
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet 으로 부터 data 추출
+			while (rs.next()) {
+				FileDto dto=new FileDto();
+				dto.setNum(rs.getInt("num"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setTitle(rs.getString("title"));
+				dto.setOrgFileName(rs.getString("orgFileName"));
+				dto.setFileSize(rs.getLong("FileSize"));
+				dto.setRegdate(rs.getString("regdate"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)rs.close();
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}
+	
+	//업로드된 파일 정보를 저장하는 메소드
+	public boolean insert(FileDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "INSERT INTO board_file"
+					+ " (num, writer, title, orgFileName, saveFileName, fileSize, regdate)"
+					+ " VALUES(board_file_seq.NEXTVAL, ?, ?, ?, ?, ?, SYSDATE)";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getTitle());
+			pstmt.setString(3, dto.getOrgFileName());
+			pstmt.setString(4, dto.getSaveFileName());
+			pstmt.setLong(5, dto.getFileSize());
+			flag = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}//insert END
+	
+	//파일 하나의 정보를 리턴하는 메소드
+	public FileDto getData(int num) {
+		//파일정보를 담을 FileDto 지역변수 선언
+		FileDto dto=null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "SELECT writer, title, orgFileName,saveFileName,"
+					+ " fileSize, regdate"
+					+ " FROM board_file"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//?에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setInt(1, num);
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet 으로 부터 data 추출
+			if (rs.next()) {
+				dto=new FileDto();
+				dto.setWriter(rs.getString("writer"));
+				dto.setTitle(rs.getString("title"));
+				dto.setOrgFileName(rs.getString("orgFileName"));
+				dto.setSaveFileName(rs.getString("saveFileName"));
+				dto.setFileSize(rs.getLong("fileSize"));
+				dto.setRegdate(rs.getString("regdate"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)rs.close();
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return dto;
+	}//getData END
+	
+}//class END
